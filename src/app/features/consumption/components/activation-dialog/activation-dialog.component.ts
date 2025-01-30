8fab4d1127820f2328e19bdd5387d0ec43b5f6f0
Mohamed Ali MDALLA <mohamed.mdalla@iliadeconsulting.com>
2025-01-30 17:03:55 +0000

    consumption popin : cancel button

--- src/app/features/consumption/components/activation-dialog/activation-dialog.component.ts
+++ src/app/features/consumption/components/activation-dialog/activation-dialog.component.ts
@@ -1,12 +1,23 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-consumption-activation-dialog',
-  imports: [ButtonModule],
+  imports: [FormsModule,ButtonModule, CheckboxModule],
   templateUrl: './activation-dialog.component.html',
   styleUrl: './activation-dialog.component.scss'
 })
 export class AppConsumptionActivationDialogComponent {
-@Output() onCloseActivationDialog:EventEmitter<void> = new EventEmitter();
+  @Output() onCloseActivationDialog: EventEmitter<void> = new EventEmitter();
+  authorizationChecked: boolean = false;
+
+  activeConsumption(){
+    if(!this.authorizationChecked){
+      return;
+    }
+
+    // TODO :  Process d'activation de consommation
+  }
 }
