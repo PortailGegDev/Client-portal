4437df58c9c7b962d2ef4b2163c7787067ff9944
Mohamed Ali MDALLA <mohamed.mdalla@iliadeconsulting.com>
2025-01-31 17:23:48 +0000

    Profile : WIP

--- src/app/features/profile/components/access-dialog/access-dialog.component.ts
+++ src/app/features/profile/components/access-dialog/access-dialog.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { SelectModule } from 'primeng/select';
+import { MultiSelectModule } from 'primeng/multiselect';
 
 @Component({
   selector: 'app-profile-access-dialog',
-  imports: [],
+  imports: [ButtonModule, SelectModule, FormsModule, MultiSelectModule],
   templateUrl: './access-dialog.component.html',
   styleUrl: './access-dialog.component.scss'
 })
 export class AppProfileAccessDialogComponent {
+  @Input() contracts: any[] = [];
+  @Input() accessDetails: any[] = [];
+  @Output() onCloseAccessDialog: EventEmitter<void> = new EventEmitter();
 
+  selectedContract: any = null;
+  selectedAccess: any[] = [];
+  contactsWithAccess: string[] = [];
+
+  onSendAccountCreation() {
+    if (this.selectedContract) {
+      // Vérifier si le contact est déjà dans la liste
+      if (!this.contactsWithAccess.includes(this.selectedContract.name)) {
+        this.contactsWithAccess.push(this.selectedContract.name);
+        localStorage.setItem('contactsWithAccess', JSON.stringify(this.contactsWithAccess));
+      }
+      this.onCloseAccessDialog.emit();
+    }
+  }
 }
--- src/app/features/profile/components/profile-details/profile-details.component.ts
+++ src/app/features/profile/components/profile-details/profile-details.component.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
 import { PanelModule } from 'primeng/panel';
+import { AppProfileAccessDialogComponent } from '../access-dialog/access-dialog.component';
 
 @Component({
   selector: 'app-profile-details',
-  imports: [FormsModule, CommonModule, PanelModule, ButtonModule],
+  imports: [FormsModule, CommonModule, PanelModule, ButtonModule, DialogModule, AppProfileAccessDialogComponent],
   templateUrl: './profile-details.component.html',
   styleUrl: './profile-details.component.scss'
 })
@@ -16,15 +18,26 @@ export class AppProfileDetailsComponent {
   isEditMode: boolean = false;
   email = 'eugenie.verret@gmail.com'; // Initial email value
   phone = '+33 6 65 43 22 11'; // Initial phone value
-  isOverlayVisible: boolean = false;
+  accessdialogVisible: boolean = false;
   contactsWithAccess: string[] = [];
+  
+  contracts = [
+    { id: 1, name: 'Valentin Verret' },
+    { id: 2, name: 'Pauline Verret' },
+    { id: 3, name: 'Jean Dupont' }
+  ];
+
+  accessDetails = [
+    { address: '16 rue Pierre Larousse, 75014 Paris', number: 'n° 1234567', details: 'Electricité - Base - 9kVA' },
+    { address: '20 rue de la Paix, 75002 Paris', number: 'n° 7654321', details: 'Gaz - Base - 12kVA' }
+  ];
 
   toggleEdit() {
     this.isEditMode = !this.isEditMode;
   }
 
-  showOverlay(): void {
-    this.isOverlayVisible = true;
+  showAccessdialog(): void {
+    this.accessdialogVisible = true;
   }
 
   saveEdit() {
@@ -34,5 +47,9 @@ export class AppProfileDetailsComponent {
   cancelEdit() {
     this.isEditMode = false;
     // Here you can add logic to revert the changes if necessary
+  }
+
+  closeAccessDialog(){
+    this.accessdialogVisible = false;
   }
 }
