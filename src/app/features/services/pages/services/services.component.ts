c08a2110aaf18efd0033cbe2fc0eb65d2c1077c7
Manar Ghmem <manar.ghmem@iliadeconsulting.com>
2025-03-27 15:40:01 +0000

    la page service avec les dernier modifs

--- src/app/features/services/pages/services/services.component.ts
+++ src/app/features/services/pages/services/services.component.ts
@@ -3,107 +3,86 @@ import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { ContractHttpService } from '../../../../core/http-services/contrat-http.service';
 import { CommonModule } from '@angular/common';
+import { ActiveContractComponent } from '../../../../shared/components/active-contract/active-contract.component';
+
 
 @Component({
   selector: 'app-services',
-  imports: [CommonModule],
+  imports: [CommonModule,ActiveContractComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 export class AppServicesComponent {
-  selectedContract: any = null;
-  countt: number | null = null;
-  contracts: any[] = [];
-  constructor(private service: ContractHttpService,
-    private router: Router,
-    private httpClient: HttpClient) { }
-  // contracts = [
-  //   { value: 'contract1', label: '16 rue Pierre Larousse, 75014 Paris Electricite, n° 1456378' },
-  //   { value: 'contract2', label: 'Contrat 2' },
-  //   { value: 'contract3', label: 'Contrat 3' },
-  //   { value: 'contract4', label: 'Contrat 4' }
-  // ];
+ 
 
-  get contractCountt(): number {
-    return this.contracts.length;
-  }
+  boxData: any[] = [
+    {
+      status: 'souscrit',
+      price: '2,99€/mois',
+      service: 'assistance dépannage',
+      title: 'Sérénité Electricité',
+      icon: '/images/Icons (1).png',
 
-  ngOnInit(): void {
-    // this.getContractData(); // Récupère le nombre de contrats
-    // this.fetchContract(); // Récupère les contrats
-  }
-  // fetchContract(): void {
-  //   this.service.fetchContract().subscribe(
-  //     data => {
-  //       console.log(data); // Vérifiez la structure des données ici
-  //       if (data && data.d && Array.isArray(data.d.results)) {
-  //         this.contracts = data.d.results;
-  //       } else {
-  //         console.error('Structure des données inattendue:', data);
-  //       }
-  //     },
-  //     error => {
-  //       console.error('Erreur lors de la récupération des données', error);
-  //     }
-  //   );
-  // }
+      backgroundImage: '/images/service_contrat.jpg'
 
+    },
+    {
+      status: 'souscrit',
+      price: '2,99€/mois',
+      service: 'assistance dépannage',
+      title: 'Sérénité Electricité',
+      icon: '/images/Icons (1).png',
+      backgroundImage: '/images/Service2.jpg'
 
-  viewDetails() {
-    this.router.navigate(['/services/green-option']);
-  }
-  consulteDetails() {
-    this.router.navigate(['/services/serenity-electricity']);
-  }
+    },
+    {
+      status: 'souscrit',
+      price: '2,99€/mois',
+      service: 'assistance dépannage',
+      title: 'Sérénité Electricité, Gaz<br>Plomberie',
+      icon: '/images/Icons (1).png',
+      backgroundImage: '/images/service3.jpg'
 
-  // contractCount: number | null = null;
-  // getContractData(): void {
-  //   this.service.fetchContractData().subscribe(
-  //     (count) => {
-  //       this.contractCount = Number(count);  // Convertir la chaîne en nombre
-  //       console.log('Number of contracts:', this.contractCount);
-  //     },
-  //     (error) => {
-  //       console.error('Error fetching contract count:', error);
-  //     }
-  //   );
-  // }
-  addressCompteur: string = '';
-  businessSectorText: string = '';
-  isOpen: boolean = false;
-  selectContract(contract: any) {
-    this.addressCompteur = contract.AddressCompteur; // Mettre à jour l'adresse sélectionnée
-    this.businessSectorText = contract.BusinessSectorText; // Mettre à jour les détails sélectionnés
-    this.isOpen = false;
-    this.selectedContract = contract;
+    },
+    {
+      status: 'souscrit',
+      price: '1€-2,99€/mois ',
+      service: 'assistance dépannage',
+      title: 'Option verte',
+      icon: '/images/Icons (1).png',
 
-  }
+      backgroundImage: '/images/service4.jpg'
 
-  toggleDropdown() {
-    this.isOpen = !this.isOpen; // Alterner l'état du menu déroulant
-  }
+    },
 
-  contractts: any[] = [];
-  // fetchContracts(): void {
-  //   this.ContractService.fetchContractISU().subscribe(
-  //     data => {
-  //       console.log('Données reçues:', data);  // Affiche les données reçues dans la console
-  //       if (data?.d?.results) {
-  //         this.contractts = data.d.results; // Récupère le tableau de résultats
-  //         console.log(this.BusinessSectorText);
-  //         if (this.contractts.length > 0) {
-  //           this.selectedContract = this.contractts[0];  // Le premier contrat sera sélectionné par défaut
+    {
+      status: 'souscrit',
+      price: 'Gratuit',
+      service: 'Service',
+      title: 'E-Facture',
+      icon: '/images/Icons (1).png',
+      badgeIcon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M6 12H18M18 12L13 7M18 12L13 17" stroke="#1E1E1E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /></svg>',
+      backgroundImage: '/images/service_facture.jpg'
+    },
+    {
+      status: 'souscrit',
+      price: 'Gratuit',
+      service: 'Service',
+      title: 'Facture en papier',
+      icon: '/images/Icons (1).png',
+      badgeIcon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M6 12H18M18 12L13 7M18 12L13 17" stroke="#1E1E1E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /></svg>',
+      backgroundImage: '/images/service6.jpg'
+    },
 
-  //         }
-  //       } else {
-  //         console.error('Aucune donnée trouvée.');
-  //         this.contractts = []; // Assure que contractts est un tableau vide si aucune donnée n'est trouvée
-  //       }
-  //     },
-  //     error => {
-  //       console.error('Erreur lors de la récupération des données:', error); // Affiche l'erreur dans la console en cas de problème
-  //       this.contractts = []; // Assure que contractts reste vide en cas d'erreur
-  //     }
-  //   );
-  // }
+    {
+      status: 'souscrit',
+      price: 'Gratuit',
+      service: 'Service',
+      title: 'Arrondis solidaires',
+      icon: '/images/Icons (1).png',
+      badgeIcon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M6 12H18M18 12L13 7M18 12L13 17" stroke="#1E1E1E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /></svg>',
+      backgroundImage: '/images/service7.jpg'
+    },
+  ];
+
 }
