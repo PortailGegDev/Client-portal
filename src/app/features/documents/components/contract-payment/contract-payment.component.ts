d488892b21ce0b6a63700c28dd9ece4df5c64669
Manar Ghmem <manar.ghmem@iliadeconsulting.com>
2025-04-07 12:34:48 +0000

    mode de paiment

--- src/app/features/documents/components/contract-payment/contract-payment.component.ts
+++ src/app/features/documents/components/contract-payment/contract-payment.component.ts
@@ -1,18 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { DialogModule } from 'primeng/dialog';
 import { InputNumberModule } from 'primeng/inputnumber';
+import { Mandate } from '../../../../shared/models/mandate.model';
+import { MaskRIBPipe } from '../../../../shared/pipe/mask-rib.pipe';
+import { TimeSpanToDatePipe } from '../../../../shared/pipe/time-span-to-date.pipe';
 
 @Component({
   selector: 'app-documents-contract-payment',
-  imports: [CommonModule, FormsModule, InputNumberModule, DialogModule, CardModule, ButtonModule],
+  imports: [CommonModule, FormsModule, InputNumberModule, DialogModule, CardModule, ButtonModule, TimeSpanToDatePipe, MaskRIBPipe,],
   templateUrl: './contract-payment.component.html',
   styleUrl: './contract-payment.component.scss'
 })
 export class AppDocumentsContractPaymentComponent {
+  @Input() mandates: Mandate[] = [];
+
   updateRib: boolean = false;
   value1: number = 1;
   
--- src/app/features/documents/pages/contract-details/contract-details.component.ts
+++ src/app/features/documents/pages/contract-details/contract-details.component.ts
@@ -10,6 +10,11 @@ import { AppDocumentsContractPaymentComponent } from '../../components/contract-
 import { AppDocumentsContractInvoiceComponent } from '../../components/contract-invoice/contract-invoice.component';
 import { AppDocumentsContractServiceComponent } from '../../components/contract-service/contract-service.component';
 import { Contract } from '../../../../shared/models/contract.model';
+import { Bank } from '../../../../shared/models/bank.model';
+import { Mandate } from '../../../../shared/models/mandate.model';
+import { MandateService } from '../../services/mandate.service';
+import { BankService } from '../../services/bank.service';
+import { AuthService } from '../../../../core/http-services/auth.service';
 
 
 
@@ -24,9 +29,12 @@ export class AppDocumentContractDetailsComponent {
   allContracts: ContractDetails[] = [];
   contractsList: Signal<Contract[]>;
   contract: Contract | undefined;
+  mandates: Mandate[] = []
 
   constructor(private router: Router,
-    private contractService: ContractService, private activatedRoute: ActivatedRoute) {
+    private contractService: ContractService, private activatedRoute: ActivatedRoute, private authService: AuthService,
+        private bankService: BankService,
+        private mandateService: MandateService) {
       this.contractsList = this.contractService.contracts;
 
 
@@ -41,6 +49,7 @@ export class AppDocumentContractDetailsComponent {
       })
     });
 
+    this.loadBankAccount();
 
   }
 
@@ -60,5 +69,47 @@ export class AppDocumentContractDetailsComponent {
 
   RetourEnBack() {
     this.router.navigate(['documents']);
+  }
+
+
+    private loadBankAccount(): void {
+    let businessPartner = this.authService.getUserData()?.bp;
+
+    if (!businessPartner) {
+      // pour tester en locale dans la DF1
+      businessPartner = '1510136444';
+      // businessPartner = '1510060117'; // bp consommation pour QF1
+      // businessPartner = '1510023652'; // bp liste de contrats pour DF1
+      // businessPartner = '1510063413'; // bp liste de contrats pour QF1
+      // businessPartner='1510031862'; // bp liste de contrats pour partenaire
+      // businessPartner='350000261'; //bp DF1
+    }
+
+
+    this.bankService.getCompteBancaire(businessPartner).subscribe({
+      next: (data: Bank[]) => {
+        if (data) {
+          let partnerBankIds = data.map(item => item.BusinessPartnerBankId);
+          this.loadMandate(partnerBankIds);;
+        }
+      },
+      error: (error: any) => {
+        console.error('Erreur lors de la récupération du compte bancaire:', error);
+      }
+    });
+  }
+
+  private loadMandate(partnerBankIds: string[]): void {
+
+    this.mandateService.getMandate(partnerBankIds).subscribe({
+      next: (data: Mandate[]) => {
+        this.mandates = data;
+        console.log('Mandates chargés:', this.mandates);
+      },
+      error: (error) => {
+        console.error('Erreur lors de la récupération du mandate:', error);
+        alert('Impossible de charger le mandat.');
+      },
+    });
   }
 }
