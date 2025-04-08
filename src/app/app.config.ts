ad07979a8f4a21bbce26802460d6218987fe9923
Mohamed Ali MDALLA <mohamed.mdalla@iliadeconsulting.com>
2025-04-08 11:37:03 +0000

    Config xsrf

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,29 +1,31 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withXsrfConfiguration } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-// import {fr} from "primelocale/fr.json";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
     providePrimeNG({
-      // translation:fr,
       theme: {
         preset: Aura,
       },
     }),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
-    //utiliser pour resoudre le pb de perte de l'application lors d'actualisation de la page
-    {
+    provideHttpClient(
+      withXsrfConfiguration({
+        cookieName: 'XSRF-TOKEN',
+        headerName: 'X-XSRF-TOKEN'
+      })
+    ),
+    {//utiliser pour resoudre le pb de perte de l'application lors d'actualisation de la page
       provide: LocationStrategy,
       useClass: HashLocationStrategy
-    } 
+    }
   ]
 };
