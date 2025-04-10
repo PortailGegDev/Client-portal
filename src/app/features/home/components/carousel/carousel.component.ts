a797d7439a36332eb8a9b1d843472f7cd940ff87
Khawla Trimeche <khawla.trimeche@iliadeconsulting.com>
2025-04-10 09:44:47 +0000

    Carousel : Les point d'indicateurs

--- src/app/features/home/components/carousel/carousel.component.ts
+++ src/app/features/home/components/carousel/carousel.component.ts
@@ -1,49 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CarouselModule } from 'primeng/carousel';
 import { VariousService } from '../../../../shared/services/various.service';
 import { Carousel } from '../../../../shared/models/carousel.model';
-import { PanelModule } from 'primeng/panel';
 import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-home-carousel',
-  imports: [PanelModule, CardModule,CarouselModule, ButtonModule],
+  imports: [CardModule,CarouselModule, ButtonModule],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss'
 })
 export class AppHomeCarouselComponent implements OnInit {
   carouselData: Carousel[] = [];
 
-  responsiveOptions: any[] | undefined;
-
   constructor(private variousService: VariousService) { }
 
   ngOnInit() {
     this.initCarouselData();
-
-    this.responsiveOptions = [
-      {
-        breakpoint: '1400px',
-        numVisible: 1,
-        numScroll: 1
-      },
-      {
-        breakpoint: '1199px',
-        numVisible: 1,
-        numScroll: 1
-      },
-      {
-        breakpoint: '767px',
-        numVisible: 1,
-        numScroll: 1
-      },
-      {
-        breakpoint: '575px',
-        numVisible: 1,
-        numScroll: 1
-      }
-    ];
   }
 
   initCarouselData() {
