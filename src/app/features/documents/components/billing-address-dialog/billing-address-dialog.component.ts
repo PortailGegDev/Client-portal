795791849ba3bd915af2b30496655a7f407f1b71
Manar Ghmem <manar.ghmem@iliadeconsulting.com>
2025-05-05 12:44:22 +0000

    les controles saisies

--- src/app/features/documents/components/billing-address-dialog/billing-address-dialog.component.ts
+++ src/app/features/documents/components/billing-address-dialog/billing-address-dialog.component.ts
@@ -2,15 +2,15 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ContractUpdate } from '../../../../shared/models/contract/contract-update.model';
 import { ContractDetails } from '../../../../shared/models/contract/contract-details.model';
 import { ContractService } from '../../../../shared/services/contract.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
-import { Contract } from '../../../../shared/models/contract/contract.model';
 import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @Component({
   selector: 'app-documents-contract-billing-address-dialog',
-  imports: [FormsModule,CommonModule,ButtonModule,InputTextModule],
+  imports: [FormsModule,CommonModule,ButtonModule,InputTextModule,ReactiveFormsModule,InputNumberModule],
   templateUrl: './billing-address-dialog.component.html',
   styleUrl: './billing-address-dialog.component.scss'
 })
@@ -22,14 +22,30 @@ export class AppDocumentsContractBillingAddressDialogComponent {
   newStreet: string = '';
   newPostalCode: string = '';
   newCity: string = '';
-  newNumber: string = '';
-  constructor(private contractService: ContractService) { }   
+  newNumber: any = '';
+  form: FormGroup;
+
+  get numberForm(): any { return this.form.get('number'); }
+  get streetForm(): any { return this.form.get('street'); }
+  get postalCodeForm(): any { return this.form.get('postalCode'); }
+  get cityForm(): any { return this.form.get('city'); }
+
+
+  constructor(private contractService: ContractService,private formBuilder: FormBuilder)  {
+    this.form = this.formBuilder.group({
+      number: ['', [Validators.required,Validators.pattern("^[0-9]*$")]],
+      street: ['', Validators.required],
+      postalCode: ['',[Validators.required,Validators.pattern(/^\d{5}$/)]],
+      city: ['', Validators.required]
+
+    });
+  }
 
     submitBillingAddress(): void {
-      const number = this.newNumber?.trim();
-      const street = this.newStreet?.trim();
-      const postalCode = this.newPostalCode?.trim();
-      const city = this.newCity?.trim();
+      const number = this.numberForm?.value;
+      const street = this.streetForm?.value;
+      const postalCode = this.postalCodeForm?.value;
+      const city = this.cityForm?.value;
     
       if (!number || !street || !postalCode || !city) {
         console.error("Tous les champs de l'adresse sont obligatoires.");
